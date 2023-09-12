f5be295d9aa0e01c4e1ae016efb064f1e638a340
Joel Lacerda <joellacerdaol@gmail.com>
2023-09-12 14:29:46 -0300

    Initial Commit

---
+++ BestGameProtagonists/BestGameProtagonistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestGameProtagonistsApp.swift
+//  BestGameProtagonists
+//
+//  Created by Joel Lacerda on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BestGameProtagonistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
